7b0c8d558dced8bf3dc2abdf39aca69f0ba138c2
Optics-Boss <->
2024-08-15 22:59:38 +0200

    Fixed bug

--- src/environment.rs
+++ src/environment.rs
@@ -1,6 +1,8 @@
 pub mod environment {
     use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
+    use crate::object::object::Object;
+
 
     pub struct Environment {
         pub enclosing: Option<Rc<RefCell<Environment>>>,
